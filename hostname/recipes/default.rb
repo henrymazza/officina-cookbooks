10c4c7a4284e12e96e18267dffbbfa786c31106c
henrymazza <fabio.mazarotto@me.com>
2011-08-03 16:33:38 -0300

    copied over hostname cookbook from joevandyk

---
+++ hostname/recipes/default.rb
@@ -0,0 +1,26 @@
+
+node[:node_hostname] = "ubuntu666.officina.me"
+node[:short_hostname] = node[:node_hostname].split('.').first
+
+
+execute "restart_hostname" do
+  command "/etc/init.d/hostname restart"
+  action :nothing
+end
+
+template "/etc/hosts" do
+  source "hosts.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :run, resources(:execute => :restart_hostname)
+end
+
+template "/etc/hostname" do
+  source "hostname.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  action :create
+  notifies :run, resources(:execute => :restart_hostname), :immediately
+end
